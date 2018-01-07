5bda0e66786b0cff8a8e963260b482fd7a38995e
Ubuntu <ubuntu@ip-172-31-32-170.ec2.internal>
2018-01-07 17:07:02 +0000

    Updates get_data

--- src/get_data_info.py
+++ src/get_data_info.py
@@ -7,7 +7,7 @@ names = ['created_at', 'hash_tags', 'coordinates',
          'user_followers', 'user_following', 'user_screen_name',
          'user_num_tweets', 'user_location']
          
-df = pd.read_csv('tweets.csv', names=names)
+df = pd.read_csv('../data/tweets.csv', names=names)
 
-print('There are {} tweets in the csv'.format(df.shape[1]))
-print('Null percentage:\n{}'.format(df.isnull().sum()/df.shape[1]))
+print('There are {} tweets in the csv\n'.format(df.shape[0]))
+print('Null percentage:\n{}'.format(df.isnull().sum()/df.shape[0]))
